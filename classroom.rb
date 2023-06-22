4398932c5944f6cf0a648e32639ba35fdb7c25ca
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-06-22 17:37:19 +0500

    complete required changes

--- classroom.rb
+++ classroom.rb
@@ -1,7 +1,8 @@
 require_relative 'student'
 
 class Classroom
-  attr_accessor :label, :student
+  attr_accessor :label
+  attr_reader :student
 
   def initialize(label)
     @label = label
--- students.rb
+++ students.rb
@@ -2,7 +2,7 @@ require_relative './person'
 require_relative './classroom'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(classroom, age, name, parent_permission: true)
     super(age, name, parent_permission)
